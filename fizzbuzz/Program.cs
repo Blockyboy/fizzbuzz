4562f452be14727177cf5ec780c9a6c7e8779fa4
Blockyboy <theblockyguy4@gmail.com>
2023-01-27 20:05:34 +1030

    Aligned code with rules of challenge, and added title card

--- fizzbuzz/Program.cs
+++ fizzbuzz/Program.cs
@@ -9,20 +9,34 @@ namespace fizzbuzz
             string fizz = "fizz";
             string buzz = "buzz";
             int numberLimit = 100;
+            bool isFizzOrBuzz = false;
+            string output = "";
 
-            for(int i = 0; i < numberLimit; ++i)
+            Console.WriteLine("FizzBuzz")
+
+            for (int i = 0; i < numberLimit; ++i)
             {
-                string output = "";
+                output = "";
+                isFizzOrBuzz = false;
                 if (i % 3 == 0)
                 {
                     output += fizz;
+                    isFizzOrBuzz = true;
                 }
                 if (i % 5 == 0)
                 {
                     output += buzz;
+                    isFizzOrBuzz = true;
                 }
 
-                Console.WriteLine(Convert.ToString(i) + ": " + output);
+                if (isFizzOrBuzz)
+                {
+                    Console.WriteLine(output);
+                }
+                else
+                {
+                    Console.WriteLine(Convert.ToString(i));
+                }
             }
         }
     }
