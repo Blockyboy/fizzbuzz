ef768b170f12e17993d798efbf21bbde89147760
Blockyboy <theblockyguy4@gmail.com>
2023-01-27 19:50:48 +1030

    Add project files.

---
+++ fizzbuzz/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fizz = "fizz";
+            string buzz = "buzz";
+
+            for(int i = 0; i < 100; ++i)
+            {
+                string output = "";
+                if (i % 3 == 0)
+                {
+                    output += "fizz";
+                }
+                if (i % 5 == 0)
+                {
+                    output += "buzz";
+                }
+
+                Console.WriteLine(Convert.ToString(i) + ": " + output);
+            }
+        }
+    }
+}
