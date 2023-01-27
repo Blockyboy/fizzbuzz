d41615ef9295f3ea812de2b8464004f1b4111858
Blockyboy <theblockyguy4@gmail.com>
2023-01-27 20:00:46 +1030

    More accessability in terms of changing variables within the code

--- fizzbuzz/Program.cs
+++ fizzbuzz/Program.cs
@@ -8,17 +8,18 @@ namespace fizzbuzz
         {
             string fizz = "fizz";
             string buzz = "buzz";
+            int numberLimit = 100;
 
-            for(int i = 0; i < 100; ++i)
+            for(int i = 0; i < numberLimit; ++i)
             {
                 string output = "";
                 if (i % 3 == 0)
                 {
-                    output += "fizz";
+                    output += fizz;
                 }
                 if (i % 5 == 0)
                 {
-                    output += "buzz";
+                    output += buzz;
                 }
 
                 Console.WriteLine(Convert.ToString(i) + ": " + output);
